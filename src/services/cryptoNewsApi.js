573f283ea7e089d4c39d2b882746494dd019c942
Bartu Gençcan <bartugenccandev@gmail.com>
2023-03-16 12:22:44 +0300

    News page done. Homepage updated.

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsApiHeaders = {
+  "x-bingapis-sdk": "true",
+  "x-rapidapi-key": "d9262bc614msheda616e6c7cbe52p134b29jsn258549a0dac9",
+  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com/news";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsApiHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
