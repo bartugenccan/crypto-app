990e7726b459e2b6faa8f41614473ef6cd39debc
Bartu Gençcan <bartugenccandev@gmail.com>
2023-03-16 11:06:51 +0300

    Cryptocurrencies component and its api service done.

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+// CSS
+import "./App.css";
+import "antd/dist/reset.css";
+
+// Router
+import { Routes, Route, Link } from "react-router-dom";
+
+// Ant Design
+import { Layout, Typography, Space } from "antd";
+
+// Components
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            CryptoHunter <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
